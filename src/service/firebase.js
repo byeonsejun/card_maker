51d3576a35d52470460b57ea9feffa42d392d710
byeonsejun <qustpwns93@gmail.com>
2022-06-08 21:32:47 +0900

    add login component

--- src/service/firebase.js
+++ src/service/firebase.js
@@ -1,5 +1,4 @@
-  // Import the functions you need from the SDKs you need
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.8.2/firebase-app.js";
+import firebase from 'firebase';
 
 const firebaseConfig = {
     apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
@@ -8,5 +7,6 @@ const firebaseConfig = {
     projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
 };
 
-  // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+export default firebaseApp;
