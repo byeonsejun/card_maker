b673df1ceaa6d131a71e894d8d899fa3a89c9e56
byeonsejun <qustpwns93@gmail.com>
2022-07-05 01:21:16 +0900

    all

--- src/app.jsx
+++ src/app.jsx
@@ -1,4 +1,4 @@
-import { HashRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import styles from './app.module.css';
 import Login from './components/login/login';
 import Maker from './components/maker/maker';
@@ -6,7 +6,7 @@ import Maker from './components/maker/maker';
 function App({ FileInput, authService, cardRepository }) {
   return (
     <div className={styles.app}>
-      <HashRouter basename={process.env.PUBLIC_URL} >
+      <BrowserRouter basename={process.env.PUBLIC_URL} >
       <Routes>
         <Route path="/" element={<Login authService={authService} />} />
         <Route path="/maker" element={
@@ -17,7 +17,7 @@ function App({ FileInput, authService, cardRepository }) {
           />
         } />
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
     </div>
   );
 }
